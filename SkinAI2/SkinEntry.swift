20f237d6021158dafab0d1053e76db6c8744d957
Natalia Chicherova <nschicherova@gmail.com>
2024-12-18 12:35:54 +0100

    added the info plist and my skin view

---
+++ SkinAI2/SkinEntry.swift
@@ -0,0 +1,18 @@
+//
+//  SkinEntry.swift
+//  SkinAI2
+//
+//  Created by Chicherova Natalia2 on 18/12/24.
+//
+
+import SwiftUI
+
+struct SkinEntry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkinEntry()
+}
---
+++ SkinAI2/SkinEntryManager.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  SkinAI2
+//
+//  Created by Chicherova Natalia2 on 18/12/24.
+//
+
